67dec4f11b5fd9788d88ace052548abd86a8407f
DEREK_SANTIAGO_GARZA__ <a.22308051281196@cbtis128.edu.mx>
2025-04-29 15:31:04 +0000

    act3

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,430 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Widget002 extends StatelessWidget {
+  const Widget002({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+    return const Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Center(
+        child: AboutListTile(
+          icon: Icon(Icons.info),
+          applicationIcon: FlutterLogo(),
+          applicationLegalese: 'Legalese',
+          applicationName: 'Flutter App',
+          applicationVersion: 'version 1.0.0',
+          aboutBoxChildren: [
+            Text('This is a text created by Flutter Mapp'),
+          ],
+        ),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class Widget004 extends StatefulWidget {
+  const Widget004({Key? key}) : super(key: key);
+
+  @override
+  State<Widget004> createState() => _Widget004State();
+}
+
+class _Widget004State extends State<Widget004> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          _showAlertDialog(context); // Llamamos a la función aquí
+        },
+        child: const Text('Show Alert Dialog'),
+      ),
+    );
+  }
+
+  // Extracting the showDialog logic into a separate method
+  void _showAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Close'),
+          ),
+        ],
+        title: const Text('Flutter Mapp'),
+        contentPadding: const EdgeInsets.all(20.0),
+        content: const Text('This is the Alert Dialog'),
+      ),
+    );
+  }
+}
+
+class Widget006 extends StatefulWidget {
+  const Widget006({Key? key}) : super(key: key);
+
+  @override
+  State<Widget006> createState() => _Widget006State();
+}
+
+class _Widget006State extends State<Widget006> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: Container(
+          width: double.infinity,
+          height: 250.0,
+          color: Colors.blueGrey,
+          child: AnimatedAlign(
+            alignment: selected ? Alignment.topRight : Alignment.bottomLeft,
+            duration: const Duration(seconds: 1),
+            curve: Curves.fastOutSlowIn,
+            child: const FlutterLogo(size: 50.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Widget008 extends StatefulWidget {
+  const Widget008({Key? key}) : super(key: key);
+
+  @override
+  State<Widget008> createState() => _Widget008State();
+}
+
+class _Widget008State extends State<Widget008> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: AnimatedContainer(
+          width: selected ? 200.0 : 100.0,
+          height: selected ? 100.0 : 200.0,
+          color: selected ? Colors.blueGrey : Colors.white,
+          alignment:
+              selected ? Alignment.center : AlignmentDirectional.topCenter,
+          duration: const Duration(seconds: 2),
+          curve: Curves.fastOutSlowIn,
+          child: const FlutterLogo(size: 75),
+        ),
+      ),
+    );
+  }
+}
+
+class Widget010 extends StatefulWidget {
+  const Widget010({Key? key}) : super(key: key);
+
+  @override
+  State<Widget010> createState() => _Widget010State();
+}
+
+class _Widget010State extends State<Widget010> {
+  bool _first = true;
+  double _fontSize = 60;
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SizedBox(
+          height: 120,
+          child: AnimatedDefaultTextStyle(
+            duration: const Duration(milliseconds: 300),
+            style: TextStyle(
+              fontSize: _fontSize,
+              color: _color,
+              fontWeight: FontWeight.bold,
+            ),
+            child: const Text('Flutter'),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            setState(() {
+              _fontSize = _first ? 90 : 60;
+              _color = _first ? Colors.blue : Colors.red;
+              _first = !_first;
+            });
+          },
+          child: const Text(
+            "Switch",
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class Widget012 extends StatefulWidget {
+  const Widget012({Key? key}) : super(key: key);
+
+  @override
+  Widget012State createState() => Widget012State();
+}
+
+class Widget012State extends State<Widget012> {
+  final _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey();
+
+  void _addItem() {
+    _items.insert(0, "Item ${_items.length + 1}");
+    _key.currentState!.insertItem(
+      0,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  void _removeItem(int index) {
+    _key.currentState!.removeItem(
+      index,
+      (_, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: const Card(
+            margin: EdgeInsets.all(10),
+            color: Colors.red,
+            child: ListTile(
+              title: Text(
+                "Deleted",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+        );
+      },
+      duration: const Duration(milliseconds: 300),
+    );
+    _items.removeAt(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 10),
+        IconButton(
+          onPressed: _addItem,
+          icon: const Icon(Icons.add),
+        ),
+        Expanded(
+          child: AnimatedList(
+            key: _key,
+            initialItemCount: 0,
+            padding: const EdgeInsets.all(10),
+            itemBuilder: (context, index, animation) {
+              return SizeTransition(
+                key: UniqueKey(),
+                sizeFactor: animation,
+                child: Card(
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.orangeAccent,
+                  child: ListTile(
+                    title: Text(
+                      _items[index],
+                      style: const TextStyle(fontSize: 24),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        _removeItem(index);
+                      },
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MyScreen extends StatelessWidget {
+  const MyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text('My Widgets Screen'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Widget002Screen()),
+                );
+              },
+              child: const Text('Show Widget 002'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Widget004Screen()),
+                );
+              },
+              child: const Text('Show Widget 004'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Widget006Screen()),
+                );
+              },
+              child: const Text('Show Widget 006'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Widget008Screen()),
+                );
+              },
+              child: const Text('Show Widget 008'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Widget010Screen()),
+                );
+              },
+              child: const Text('Show Widget 010'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const Widget012Screen()),
+                );
+              },
+              child: const Text('Show Widget 012'),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+class Widget002Screen extends StatelessWidget {
+  const Widget002Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget 002')),
+      body: const Widget002(),
+    );
+  }
+}
+
+class Widget004Screen extends StatelessWidget {
+  const Widget004Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget 004')),
+      body: const Widget004(),
+    );
+  }
+}
+
+class Widget006Screen extends StatelessWidget {
+  const Widget006Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget 006')),
+      body: const Widget006(),
+    );
+  }
+}
+
+class Widget008Screen extends StatelessWidget {
+  const Widget008Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget 008')),
+      body: const Widget008(),
+    );
+  }
+}
+
+class Widget010Screen extends StatelessWidget {
+  const Widget010Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget 010')),
+      body: const Widget010(),
+    );
+  }
+}
+
+class Widget012Screen extends StatelessWidget {
+  const Widget012Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget 012')),
+      body: const Widget012(),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyScreen(),
+  ));
 }
